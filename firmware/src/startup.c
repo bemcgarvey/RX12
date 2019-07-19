7c549f08de1d185f699a3483ce6836d8b90aacd1
bemcgarvey <bemcgarvey@comcast.net>
2019-07-19 09:10:30 -0600

    Improved startup detection.
    Added better delays and ensured no short across bind pins

--- firmware/src/startup.c
+++ firmware/src/startup.c
@@ -1,5 +1,6 @@
 #include <xc.h>
 #include "startup.h"
+#include "timers.h"
 
 StartupMode startupMode = START_NORMAL;
 
@@ -7,19 +8,18 @@ void DetectStartupMode(void) {
     startupMode = START_NORMAL;
     ANSELBbits.ANSB0 = 0;
     ANSELAbits.ANSA8 = 0;
+    LATAbits.LATA1 = 0;
+    LATBbits.LATB0 = 0;
     TRISBbits.TRISB0 = 1;
     CNPDBbits.CNPDB0 = 1; //enable pull-down
     TRISAbits.TRISA1 = 0;
-    LATAbits.LATA1 = 0;
-    Nop();
-    Nop();
+    delay_us(1);
     if (PORTBbits.RB0 == 1) {
         startupMode = START_SERIAL;
     } else {
         CNPDBbits.CNPDB0 = 0;
         CNPUBbits.CNPUB0 = 1; //enable pull-up
-        int i;
-        for (i = 0; i < 100; ++i);
+        delay_us(1);
         if (PORTBbits.RB0 == 0) {
             startupMode = START_BIND;
         }
